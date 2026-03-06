c60996bb7c97ccc00499ef8d4624b90c91362051
abhinandan.kumar <129488983+abhiraj2512@users.noreply.github.com>
2026-03-06 19:08:25 +0530

    Fix the JWt bug

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { User } from '../users/entities/user.entity';
@@ -9,9 +10,13 @@ import { User } from '../users/entities/user.entity';
 @Module({
     imports: [
         PassportModule,
-        JwtModule.register({
-            secret: process.env.JWT_SECRET,
-            signOptions: { expiresIn: '1h' },
+        JwtModule.registerAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                secret: configService.get<string>('JWT_SECRET'),
+                signOptions: { expiresIn: '1h' },
+            }),
         }),
         TypeOrmModule.forFeature([User]),
     ],
